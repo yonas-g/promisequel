da4be8fcc6aaffcd77c81d0e7188c9b88289778b
yonas-g <yonasgebeyaw1990@gmail.com>
2019-08-03 21:05:06 +0300

    Initial Mysql Promise wrapper npm package

---
+++ index.js
@@ -0,0 +1,27 @@
+class Database {
+    constructor(config) {
+        this.config = config
+
+        this.connection = mysql.createConnection(config);
+    }
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err)
+                    return reject(err);
+                resolve();
+            });
+        });
+    }
+}
+
+module.exports = Database
